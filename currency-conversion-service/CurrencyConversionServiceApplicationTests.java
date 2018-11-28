68936f6a25d8f4a8b582102080e16b605339705b
priyankarakshit <38922482+priyankarakshit@users.noreply.github.com>
2018-11-29 00:04:38 +0530

    Add files via upload

--- currency-conversion-service/CurrencyConversionServiceApplicationTests.java
+++ currency-conversion-service/CurrencyConversionServiceApplicationTests.java
@@ -1,22 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.microservices.currencyconversionservice;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-/**
- *
- * @author piku
- */
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CurrencyConversionServiceApplicationTests {
-     @Test
- public void contextLoads() {
- }
+
+	@Test
+	public void contextLoads() {
+	}
 
 }
