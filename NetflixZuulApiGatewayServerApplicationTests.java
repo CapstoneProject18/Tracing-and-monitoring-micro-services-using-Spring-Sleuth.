8c1e797168e50523ef1a3c4bbca2a0ff3f247fb4
saipraneeth596 <saipraneeth596@gmail.com>
2018-11-29 00:40:09 +0530

    Add files via upload

---
+++ NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.microservices.netflixzuulapigatewayserver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author piku
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class NetflixZuulApiGatewayServerApplicationTests {
+    @Test
+ public void contextLoads() {
+ }
+
+}
